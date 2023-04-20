e5f854a2e18c8ca88d4410f24a5776ef957a8154
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-04-20 15:50:42 -0700

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
 """
-determines if a given data set represents a valid UTF-8 encoding
-returns True if data is a valid UTF-8 encoding
-returns False if data is not valid UTF-8 encoding
+Checks and validates UTF-8 encoding
+
 """
+
+def decimalToBinary(no):
+    #converting decimal to binary
+    return bin(no).replace("0b", "")
 
 
 def validUTF8(data):
 
-    number_of_bytes = 0  # number of bytes in the current UTF-8 char
-    # goes into the numbers of the given set
-    for numbers in data:
-        # prints the 8least sig bits
-        number_of_bin = format(numbers, '#010b')[-8:]
-        if number_of_bytes == 0:  # start processing a new UTF-8 char
+    no_of_bytes = 0 
+    
+    for no in data:
+        # the least significant bits of the last block
+        bits_per_byte = decimalToBinary(no)
+        if no_of_bytes == 0:
 
-            # Get the number of 1s in the beginning of the string.
-            for bit in number_of_bin:
+            for bit in bits_per_byte:
                 if bit == '0':
                     break
-                number_of_bytes += 1
+                no_of_bytes += 1
 
-            if number_of_bytes == 0:  # 1 byte characters
+            if no_of_bytes == 0:
                 continue
 
-            # A character in UTF-8 can be 1 to 4 bytes long
-            if number_of_bytes == 1 or number_of_bytes > 4:
+            if no_of_bytes == 1 or no_of_bytes > 4:
                 return False
         else:
 
-            # Bytes which are a part of
-            # a UTF-8 character must adhere
-            # to the pattern `10xxxxxx`.
-            if not number_of_bin.startswith('10'):
+            if not bit_per_byte.startswith('10'):
                 return False
 
-        # We reduce the number of bytes
-        # to process by 1 after each integer.
-        number_of_bytes -= 1
+        no_of_bytes -= 1
 
-    return number_of_bytes == 0
+    return no_of_bytes == 0
