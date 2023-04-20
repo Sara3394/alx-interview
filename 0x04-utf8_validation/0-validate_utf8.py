5eeafd8a973e028949107f4bda896c81f80e2d96
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-04-20 15:59:54 -0700

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,27 +6,26 @@ checks and validates UTF-8 encoding
 
 def validUTF8(data):
 
-    number_of_bytes = 0
+    no_of_bytes = 0
     for numbers in data:
-        # prints the 8least sig bits
         number_of_bin = format(numbers, '#010b')[-8:]
-        if number_of_bytes == 0:
+        if no_of_bytes == 0:
 
             for bit in number_of_bin:
                 if bit == '0':
                     break
-                number_of_bytes += 1
+                no_of_bytes += 1
 
-            if number_of_bytes == 0:
+            if no_of_bytes == 0:
                 continue
 
-            if number_of_bytes == 1 or number_of_bytes > 4:
+            if no_of_bytes == 1 or no_of_bytes > 4:
                 return False
         else:
 
             if not number_of_bin.startswith('10'):
                 return False
 
-        number_of_bytes -= 1
+        no_of_bytes -= 1
 
-    return number_of_bytes == 0
+    return no_of_bytes == 0
