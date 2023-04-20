248af1c6d9fbbc703ad974403524fbe57d561985
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-04-20 16:09:32 -0700

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,7 @@ def validUTF8(data):
 
     no_of_bytes = 0
     for no in data:
-        bits_per_byte_block = format(numbers, '#010b')[-8:]
+        bits_per_byte_block = format(no, '#010b')[-8:]
         if no_of_bytes == 0:
 
             for bit in bits_per_byte_block:
