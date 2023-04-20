c55c8561203c25be8220067fac1c9cdef11ad5ea
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-04-20 15:57:38 -0700

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,22 +17,16 @@ def validUTF8(data):
                     break
                 number_of_bytes += 1
 
-            if number_of_bytes == 0:  # 1 byte characters
+            if number_of_bytes == 0:
                 continue
 
-            # A character in UTF-8 can be 1 to 4 bytes long
             if number_of_bytes == 1 or number_of_bytes > 4:
                 return False
         else:
 
-            # Bytes which are a part of
-            # a UTF-8 character must adhere
-            # to the pattern `10xxxxxx`.
             if not number_of_bin.startswith('10'):
                 return False
 
-        # We reduce the number of bytes
-        # to process by 1 after each integer.
         number_of_bytes -= 1
 
     return number_of_bytes == 0
