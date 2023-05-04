215d0512489762d784fc736a1aabd4a3925d918e
Sara <Sara3394@github.com>
2023-05-04 10:37:52 -0700

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 
+const { ServerResponse } = require('http');
 const request = require('request');
 const movies = process.argv[2];
 
 request.get(`https://swapi-api.hbtn.io/api/films/${movies}/`, async (_error, response, body) => {
+    _error && console.log(_error);
 
-  const arrayofcharacters = (JSON.parse(res.body).characters);
+  const arrayofcharacters = (JSON.parse(ServerResponse.body).characters);
   for (const character of arrayofcharacters) {
-    const ppl = await new Promise((resolve, reject) => {
-        request(character, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(body).name);
-          }
+    await new Promise((resolve, reject) => {
+        request(character, (err, res, body) => {
+            _error && console.log(_error);
+  
+          console.log(JSON.parse(body).name);
+          resolve();
         });
       });
-      console.log(ppl);
     }
   });
+   
