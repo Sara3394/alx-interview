484f3137176607d66ae011225360d5a142814898
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-04 11:45:42 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,18 @@
+#!/usr/bin/node
 
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  for (const character of JSON.parse(body).characters) {
+    const ppl = await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
+    console.log(ppl);
+  }
+});
