f8d30e3cdfc595fd38bcc7128ff6cad957b3da40
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-07 12:38:27 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movies = process.argv[2];
+
 request('https://swapi-api.alx-tools.com/api/films/' + id, async function (error, res, body) {
   
   const actors = JSON.parse(body).characters;
