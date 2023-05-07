d7bca4eafd492e72758677fe6dad072ab0876cf0
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-07 12:41:44 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,12 +3,12 @@
 const request = require('request');
 const movies = process.argv[2];
 
-request('https://swapi-api.alx-tools.com/api/films/' + id, async function (error, res, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + id, async function (error, response, body) {
   
   const actors = JSON.parse(body).characters;
   for (const character of actors) {
     await new Promise((resolve, reject) => {
-      request(character, (error, res, body) => {
+      request(character, (error, response, body) => {
         if (error) {
           console.log(error);
         }
