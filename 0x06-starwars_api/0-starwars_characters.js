533967cc2f24f5ce48752da6c4504ec0e402e5c7
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-07 12:25:30 -0700

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
-
-// this is a script which prints all characters of a Star Wars movie
+// this is a script that prints all characters of a Star Wars movie
 
 const request = require('request');
+const id = process.argv[2];
 
-const movie = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movie}`;
-
-request(url, async (err, res, body) => {
-  if (err) {
-    console.log(err);
-  }
-  for (const character of JSON.parse(body).characters) {
+request('https://swapi-api.alx-tools.com/api/films/' + id, async function (err, res, body) {
+  if (err) throw err;
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
     await new Promise((resolve, reject) => {
       request(character, (err, res, body) => {
         if (err) {
-          reject(err);
+          console.log(err);
         }
         console.log(JSON.parse(body).name);
         resolve();
