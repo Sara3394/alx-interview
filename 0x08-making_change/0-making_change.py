0767543cbb3b69c29c3002f38c06f8a3bf222352
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-16 07:16:41 -0700

    Update 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values,
+this script finds the minimum number of coins 
+needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """ Returns: fewest number of coins needed to meet total """
+    if total <= 0:
+        return 0
+    if len(coins) <= 0:
+        return -1
+    coinsNeeded = 0
+    coins.sort()
+    coins.reverse()
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            coinsNeeded += 1
+        if (total == 0):
+            return coinsNeeded
+    return -1
