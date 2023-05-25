50890755e24193041730601676a2840367d424a6
Sara3394 <hailu.serafina@yahoo.com>
2023-05-25 17:31:07 +0000

    task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,7 +12,6 @@ def island_perimeter(grid):
     Return:
         The perimeter of the island
     """
-    
     p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -22,4 +21,4 @@ def island_perimeter(grid):
                     p -= 2
                 if j > 0 and grid[i][j-1] == 1:
                     p -= 2
-    return p
+                    return p
